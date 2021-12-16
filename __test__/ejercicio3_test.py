50abe902e7061f9f8685c1f27f3d95b263a5ee33
Arabath <axelhobrecht@hotmail.com>
2021-12-16 00:01:51 -0300

    Ejercicio 4 + Tests (completados)

--- __test__/ejercicio3_test.py
+++ __test__/ejercicio3_test.py
@@ -1,23 +1,47 @@
 import unittest
 from ejercicio3 import Punto
-from excepcion_punto import DistanciaCero
-class TestPunto(unittest.TestCase):
-    """ Suite-Test de la clase Punto """
+from ejercicio4 import Circulo
+
+class TestCirculo(unittest.TestCase):
+    """ Suite-Test de la clase Circulo """
     def setUp(self) -> None:
-        self.punto1 = Punto(0, 1)
-        self.punto2 = Punto(0, 1)
+        self.punto1 = Punto(4, 2)
+        self.circulo1 = Circulo(self.punto1, 2)
+        self.punto2 = Punto(4, 3)
+        self.circulo2 = Circulo(self.punto2, 2)
 
-    def test_funcionEsIgual(self):
-        """ Verifica que dos puntos sean iguales """
+    def test_metodoPuntoDentroDeCirculo(self):
+        """ Verifica si un punto se encuentra dentro de un circulo determinado """
 
-        self.assertTrue(self.punto1.esIgual(self.punto2))
+        self.assertTrue(self.circulo1.puntoDentroDeCirculo(self.punto2), True)
 
-    def test_funcionDistanciaA(self):
-        """ Verifica la distancia entre dos puntos """
-        punto1 = Punto(8, 15)
-        punto2 = Punto(2, 3)
 
-        self.assertAlmostEquals(punto1.distanciaA(punto2), 13.42, places=2)
+    def test_metodoCirculosIgualesSegunRadio(self):
+        """ Verifica si dos circulos son iguales entorno a su radio """
 
-if __name__ == "__main__":
-    unittest.main()
+        self.assertTrue(self.circulo1.circulosIgualesSegunRadio(self.circulo2))
+
+    def test_metodoCirculoContieneOtroCirculo(self):
+        """ Verifica si un circulo contiene otro circulo dentro """
+        punto1 = Punto(4, 2)
+        circulo1 = Circulo(punto1, 2)
+        punto2 = Punto(4, 2)
+        circulo2 = Circulo(punto2, 1)
+
+        self.assertTrue(circulo1.circuloContieneOtroCirculo(circulo2))
+
+    def test_metodoCirculosSuperpuestos(self):
+        punto1 = Punto(4, 2)
+        circulo1 = Circulo(punto1, 2)
+        punto2 = Punto(4, 2)
+        circulo2 = Circulo(punto2, 2)
+
+        self.assertTrue(circulo1.circulosSuperpuestos(circulo2))
+
+    def test_metodocirculosSeIntersecan(self):
+        punto1 = Punto(4, 2)
+        circulo1 = Circulo(punto1, 1)
+        punto2 = Punto(4, 2)
+        circulo2 = Circulo(punto2, 2)
+
+        self.assertTrue(circulo1.circulosSeIntersecan(circulo2))
--- ejercicio4.py
+++ ejercicio4.py
@@ -1,49 +1,99 @@
 """
     Ejercicio 4:
-        La empresa para la que usted trabaja planea desarrollar un software para la visualización
-         de figuras geométricas. Para esto se le pide que:
+        La empresa para la que usted trabaja planea desarrollar un software para la visualización de figuras geométricas. Para esto se le pide que:
         a) Modele el concepto de un círculo en términos de su centro y su radio.
         b) Pueda saber si un punto se encuentra dentro de un círculo.
         c) Pueda saber si dos circulos son iguales en torno a su radio.
         d) Pueda saber si un circulo contiene otro circulo.
         e) Pueda saber si dos circulos estan superpuestos.
         f) Pueda saber si dos círculos se intersecan o no.
-        g) Generar una excepción en el caso de que el radio pasado como parametro 
+        g) Generar una excepción en el caso de que el radio pasado como parametro.
             sea menor a 1. ValueError('Radio no valido!.')
-
         Implemente las clases necesarias y sus tests.
         Puede reutilizar la clase Punto.
 """
+
 from ejercicio3 import Punto
+import math
 class Circulo():
-    """ Clase circulo """
+    """
+        Clase Cirulo:
+        [+] Recibe 2 parámetros
+            - Clase Punto(x,y) con sus coordenadas.
+            - radio del circulo.
+    """
 
     def __init__(self, punto: 'Punto', radio: int) -> None:
         self.__punto = punto
         self.__radio = radio
 
     @property
-    def cordenadaX(self): return self.__punto.cordenadaX
-    @property
-    def cordenadaY(self): return self.__punto.cordenadaY
+    def radio(self):
+        """ [+] Retorna el radio del circulo seleccionado """
+        return self.__radio
 
     @property
-    def radio(self): return self.__radio
+    def perimetro(self):
+        """ [+] Retorna el perimetro del circulo seleccionado """
+        return self.__radio * math.pi
 
 
-if __name__ == "__main__":
-    punto1 = Punto(3,2)
-    circulo1 = Circulo(punto1, 6)
-    punto2 = Punto(2,2)
+    # b) Pueda saber si un punto se encuentra dentro de un círculo.
+    def puntoDentroDeCirculo(self, punto: 'Punto') -> bool:
+        """ [+] Verifica si un punto se encuentra dentro de un circulo """
+        distancia = self.__punto.distanciaA(punto)
+        if distancia < self.__radio:
+            return True
+        else:
+            return False
+
+
+    # c) Pueda saber si dos circulos son iguales en torno a su radio.
+    def circulosIgualesSegunRadio(self, circulo: 'Circulo') -> bool:
+        """ [+] Verifica si dos circulos son igual entorno a su radio """
+        return self.__radio == circulo.radio
+
+
+    # d) Pueda saber si un circulo contiene otro circulo.
+    def circuloContieneOtroCirculo(self, circulo: 'Circulo') -> bool:
+        """ [+] Verifica si el circulo contiene otro circulo dentro """
+        return (self.__punto.distanciaA(circulo.__punto) + circulo.__radio) < self.__radio
+
+
+    # e) Pueda saber si dos circulos estan superpuestos.
+    def circulosSuperpuestos(self, circulo: 'Circulo') -> bool:
+        """ [+] Verifica si los circulos se encuentran superpuestos """
+        return self.__punto.esIgual(circulo.__punto) and self.circulosIgualesSegunRadio(circulo)
+
+
+    # f) Pueda saber si dos círculos se intersecan o no.
+    def circulosSeIntersecan(self, circulo: 'Circulo') -> bool:
+        """ [+] Verifica si los circulos se intersecan """
+        return not Circulo(self.__punto, self.__radio).circuloContieneOtroCirculo(circulo) and not Circulo(self.__punto, self.__radio).circulosSuperpuestos(circulo) and self.__punto.distanciaA(circulo.__punto) < (self.__radio + circulo.__radio)
+
+
+if __name__ == '__main__':
+    punto1 = Punto(4, 2)
+    circulo1 = Circulo(punto1, 2)
+
+    punto2 = Punto(7, 2)
     circulo2 = Circulo(punto2, 2)
 
-    
-print(circulo1.cordenadaX)
-print(circulo1.cordenadaY)
-print(circulo1.radio)
-
-print(circulo2.cordenadaX)
-print(circulo2.cordenadaY)
-print(circulo2.radio)
-
-
+    print(f"[+] CoordenadaX del punto1: {punto1.cordenadaX}")
+    print(f"[+] CoordenadaY del punto1: {punto1.cordenadaY}")
+    print(f"[+] Radio del circulo del punto1: {circulo1.radio}")
+    print(f"[+] Perimetro del circulo del punto1 : {circulo1.perimetro}")
+    print("-----")
+    print(f"[+] CoordenadaX del punto2: {punto2.cordenadaX}")
+    print(f"[+] CoordenadaY del punto2: {punto2.cordenadaY}")
+    print(f"[+] Distancia entre puntos: {punto1.distanciaA(punto2)}")
+    print("-----")
+    print(f"[+] Punto dentro del circulo?: {circulo1.puntoDentroDeCirculo(punto2)}")
+    print("-----")
+    print(f"[+] Circulos iguales segun radio?: {circulo1.circulosIgualesSegunRadio(circulo2)}")
+    print("-----")
+    print(f"[+] Circulos contiene otro circulo?: {circulo1.circuloContieneOtroCirculo(circulo2)}")
+    print("-----")
+    print(f"[+] Circulos se encuetran superpuestos?: {circulo1.circulosSuperpuestos(circulo2)}")
+    print("-----")
+    print(f"[+] Circulos se intersecan?: {circulo1.circulosSeIntersecan(circulo2)}")
